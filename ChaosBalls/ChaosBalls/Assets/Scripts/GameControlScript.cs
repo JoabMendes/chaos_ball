af3a898d0b6b525cc1098b762beb13f27824fa4e
JoabMendes <joab.mendes.r2d2@gmail.com>
2016-08-23 13:26:55 -0300

    First commit

---
+++ ChaosBalls/ChaosBalls/Assets/Scripts/GameControlScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControlScript : MonoBehaviour {
+
+
+	public GameObject blueBall, greenBall, redBall, orageBall;
+
+	private bool isGameOver = false;
+	
+	// Update is called once per frame
+	void Update () {
+		//If all four ball are not (!) existent then game is over
+		isGameOver = !blueBall && !greenBall && !redBall && !orageBall;
+		//The balls are destroyed when collide with the lights goals
+	}
+
+	void OnGUI(){
+		if (isGameOver) {
+			Rect rect = new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 110);
+			GUI.Box (rect, "Game Over");
+
+			Rect rect2 = new Rect (Screen.width / 2 - 30, Screen.height / 2 - 25, 200, 110);
+			GUI.Label (rect2, "Good Job!\n\nCredits:\nJoab Mendes");
+		}
+	
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# chaos_ball
-Chaos Ball Game
+# Chaos Ball Game
+
+Prática dos conceitos de modelagem de jogos 3D em Unity.
+
+# The Concept
+
+This is a game slightly akin to Pinball or Breakout. The player will be in an arena. Each of the 
+four corners will have a color, and four balls with corresponding colors will be floating around. 
+Amid the four colored balls, there will be several yellow balls, called chaos balls. 
+Chaos balls exist solely to get in your way and make the game challenging! They are smaller than the four 
+colored balls, but they also move faster. Players will have a flat surface with which they will 
+attempt to knock the colored balls into the correct corners. 
+
